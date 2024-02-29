24e39d91171d9f384933a4c6828f736947442855
baldeeb <baldeeb@umich.edu>
2024-02-29 13:49:10 -0500

    Updates & cleans unet training.

---
+++ models/unets.py
@@ -0,0 +1,66 @@
+import torch
+from diffusers import UNet2DModel, UNet2DConditionModel
+
+UNET_DEFAULTS = {
+    'block_out_channels': (128, 128, 128), # the number of output channels for each UNet block
+    'down_block_types': ("DownBlock2D", "DownBlock2D", "DownBlock2D",), # regular ResNet downsampling block
+    'up_block_types': ("UpBlock2D", "UpBlock2D", "UpBlock2D",) # regular ResNet upsampling block
+}
+CONDITIONED_UNET_DEFAULTS = UNET_DEFAULTS
+
+def get_unet(cfg):
+    for k in UNET_DEFAULTS:
+        if k not in cfg:
+            cfg[k] = UNET_DEFAULTS[k]
+
+    return UNet2DModel(
+        sample_size=cfg['image_size'],  # the target image resolution
+        in_channels=3,  # the number of input channels, 3 for RGB images
+        out_channels=3,  # the number of output channels
+        layers_per_block=1,  # how many ResNet layers to use per UNet block
+        block_out_channels=cfg['block_out_channels'],  
+        down_block_types=cfg['down_block_types'],
+        up_block_types=cfg['up_block_types']
+    )
+
+def get_conditioned_unet(cfg):
+    for k in CONDITIONED_UNET_DEFAULTS:
+        if k not in cfg:
+            cfg[k] = CONDITIONED_UNET_DEFAULTS[k]
+
+    return UNet2DConditionModel(
+        sample_size=cfg['image_size'],
+        in_channels=3,
+        out_channels=3,
+        layers_per_block=1,
+        block_out_channels=cfg['block_out_channels'],  
+        down_block_types=cfg['down_block_types'],
+        up_block_types=cfg['up_block_types']
+    )
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda:0')
+    net = get_conditioned_unet({'image_size':32}).to(device=device)
+    images = torch.rand(2, 3, 32, 32, device=device)
+    timesteps = torch.tensor([100, 100], dtype=torch.long, device=device)
+    ctxts = torch.rand(2, 1, 1280, device=device)
+    data = (images, timesteps, ctxts)
+    result = net(*data).sample
+
+    kwargs = {'input_names':'data', 'output_names':'results'}
+
+    torch.onnx.export(net, 
+                    #   {
+                    #     'sample': images,
+                    #     'timestep': timesteps,
+                    #     'encoder_hidden_states': ctxts,
+                    #   },
+                      (images, timesteps, ctxts), 
+                      'rnn.onnx', 
+                      input_names=['data', 'timesteps', 'context'], 
+                      output_names=['results'],
+                    #   dynamic_axes={'input' : {0 : 'batch_size'},
+                    #                 'output' : {0 : 'batch_size'}}
+                    )
+    # torch.onnx.export(net, *data, 'rnn.onnx', **kwargs)
--- train_huggingface.py
+++ train_huggingface.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn.functional as F
 
 from models.nocs_diffusion import NocsDiff, CtxtEncoder
-
 from models.scheduler import VarianceSchedule
+from models.unets import get_unet
+
 from utils.nocs_renderer import RendererWrapper, sample_transforms, mask_from_depth
 from utils.dataloader import PointCloudLoader
 from utils.nocs_generator import nocs_extractor
@@ -21,22 +22,25 @@ def train():
 
     ######## Configs ############
     class Config:
-        device       = 'cuda'
+        device = 'cuda'
 
         # Augmentation
         image_size=32
-        num_views = 4
+        num_views = 16
         
         # Training
-        batch_size   = 2
+        batch_size = 2
         lr = 1e-4
         lr_warmup_steps = 50
         num_epochs = 10000
         num_train_timesteps = 1000
+
+        def as_dict(self,):
+            return dict((name, getattr(self, name)) for name in dir(self) if not name.startswith('__')) 
     config = Config()
     #############################
 
-    # Stup Dataset
+    # Setup Dataset
     dataloader = PointCloudLoader(
                             path='./data/shapenet.hdf5',
                             categories=['chair'],
@@ -55,40 +59,15 @@ def train():
     
     
     # model = NocsDiff(3, ctx_net, 64).to(config.device)  # MY OWN
-
-    from diffusers import UNet2DModel
-    model = UNet2DModel(
-        sample_size=config.image_size,  # the target image resolution
-        in_channels=3,  # the number of input channels, 3 for RGB images
-        out_channels=3,  # the number of output channels
-        layers_per_block=1,  # how many ResNet layers to use per UNet block
-        # block_out_channels=(128, 128, 256, 256, 512, 512),  # the number of output channels for each UNet block
-        block_out_channels=(128, 128),  # the number of output channels for each UNet block
-        down_block_types=(
-            "DownBlock2D",  # a regular ResNet downsampling block
-            # "DownBlock2D",
-            # "DownBlock2D",
-            # "DownBlock2D",
-            # "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
-            "DownBlock2D",
-        ),
-        up_block_types=(
-            "UpBlock2D",  # a regular ResNet upsampling block
-            # "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
-            # "UpBlock2D",
-            # "UpBlock2D",
-            # "UpBlock2D",
-            "UpBlock2D",
-        ),
-    )
+    model = get_unet(config.as_dict())
     model = model.to(config.device)
 
     render = RendererWrapper(image_size=config.image_size)
 
     
-    optim = torch.optim.AdamW(model.parameters(), lr=config.lr)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=config.lr)
     lr_scheduler = get_cosine_schedule_with_warmup(
-        optimizer=optim,
+        optimizer=optimizer,
         num_warmup_steps=config.lr_warmup_steps,
         num_training_steps=(config.num_epochs),
     )
@@ -102,22 +81,23 @@ def train():
     pts = pts[0]
     feats = (feats[0] - 0.5) * 2  # try range (-1, +1)
     
-    Rs, Ts = sample_transforms(config.num_views, elev_range=[0, 70])
-    renders = render(pts, feats, Rs=Rs, Ts=Ts)
-    images, depths = renders['images'], renders['depths']
-    
-    # TEMP TEST: Setting all images to be the same. noise will be differen.
-    images[1:] = images[0:1]
-    depths[1:] = depths[0:1]
-    
-    images = images.permute(0, 3, 1, 2).to(config.device)
-    depths = depths.permute(0, 3, 1, 2).to(config.device)
-    masks = mask_from_depth(depths)
-
-    # VISUALIZE DATA
-    # viz_image_batch(images.permute(0,2,3,1).detach().cpu().numpy())
-
     for epoch in range(config.num_epochs):
+
+        Rs, Ts = sample_transforms(config.num_views, elev_range=[0, 70])
+        renders = render(pts, feats, Rs=Rs, Ts=Ts)
+        images, depths = renders['images'], renders['depths']
+        
+        # # TEMP TEST: Setting all images to be the same. noise will be differen.
+        # images[1:] = images[0:1]
+        # depths[1:] = depths[0:1]
+        
+        images = images.permute(0, 3, 1, 2).to(config.device)
+        depths = depths.permute(0, 3, 1, 2).to(config.device)
+        masks = mask_from_depth(depths)
+
+        # VISUALIZE DATA
+        # viz_image_batch(images.permute(0,2,3,1).detach().cpu().numpy())
+
 
         # forward diffusion
         timesteps = torch.randint(
@@ -136,21 +116,24 @@ def train():
         # print loss
         if epoch % 500 == 0: print(loss)
         
-        optim.step()
+        optimizer.step()
         lr_scheduler.step()
-        optim.zero_grad()
+        optimizer.zero_grad()
+
+
+    as_np = lambda x: (x/2 + 0.5).permute(0,2,3,1).detach().cpu().numpy()
+
+    viz_image_batch(as_np(images), block=False, title='Original')
+    viz_image_batch(as_np(noised_images), block=False, title='Noised')
 
     img = noised_images.clone().detach()
-    viz_image_batch(img.permute(0,2,3,1).detach().cpu().numpy())
-
     for t in scheduler.timesteps:
         with torch.no_grad():
             # noisy_residual = model(input, depths)  # MY OWN
-            noisy_residual = model(img, t).sample  # Unet
+            noisy_residual = model(img, t).sample  # UNet
         previous_noisy_sample = scheduler.step(noisy_residual, t, img).prev_sample
         img = previous_noisy_sample
     
-    img = img/2 + 0.5
-    viz_image_batch(img.permute(0,2,3,1).detach().cpu().numpy())
+    viz_image_batch(as_np(img), title='Fixed')
     pass
 train()
