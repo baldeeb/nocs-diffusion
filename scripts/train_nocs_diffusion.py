7efeca4c3c6b8559faca1093ef3c633dfbacdfcd
baldeeb <baldeeb@umich.edu>
2024-09-08 19:00:08 -0400

    reverts to using hydra  command line to run training.

--- scripts/train_nocs_diffusion.py
+++ scripts/train_nocs_diffusion.py
@@ -3,59 +3,30 @@ from diffusers.optimization import get_cosine_schedule_with_warmup
 from nocs_diffusion import train, ConfigDirectoriesManager, ConfigLoader
 from nocs_diffusion.utils.load_save import save_model
 
-from omegaconf import OmegaConf
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
 import wandb
 import os
 from tqdm import tqdm
 import pathlib as pl
 
 
-if __name__ == '__main__':
-    cfg_path = ConfigDirectoriesManager()['train_diffusion.yaml']
-    loader = ConfigLoader.from_config_path(str(cfg_path))
-    cfg = loader.cfg
-    dataloader = loader.dataloader
-    model = loader.model
+@hydra.main(version_base=None, config_path='../src/nocs_diffusion/config', config_name='train_diffusion')
+def run(cfg: DictConfig) -> None:
+    model = hydra.utils.instantiate(cfg.model).to(cfg.device)
+    dataloader = hydra.utils.instantiate(cfg.dataloader).to(cfg.device)
     
     # TODO: move optimizer to hydra config.
-    optimizer = torch.optim.AdamW(model.diff_net.parameters(), lr=loader.cfg.lr)
+    optimizer = torch.optim.AdamW(model.diff_net.parameters(), lr=cfg.lr)
     lr_scheduler = get_cosine_schedule_with_warmup(
         optimizer=optimizer,
-        num_warmup_steps=loader.cfg.lr_warmup_steps,
-        num_training_steps=(loader.cfg.num_steps),
+        num_warmup_steps=cfg.lr_warmup_steps,
+        num_training_steps=(cfg.num_steps),
     )
-    train(loader.cfg, model, optimizer, lr_scheduler, dataloader)
-    
-    # Logger
-    if cfg.log: 
-        wandb.init(**cfg.logger, config=OmegaConf.to_container(cfg))
-        log = wandb.log
-        if cfg.log_locally: os.environ["WANDB_MODE"] = "offline"
-    else: log = lambda _: None
 
-    checkpoint_i = 0
-    batch_tqdm = tqdm(range(cfg.num_steps), 
-                      desc='Training Step Loop')
-    for batch_i in batch_tqdm:
-        log({'step': batch_i+1})
+    train(cfg, model, optimizer, lr_scheduler, dataloader)
 
-        data = dataloader()
-        loss = model.loss(**data)
-        log(loss)
-        
-        optimizer.zero_grad()
-        loss['loss'].backward()
-        optimizer.step()
-        lr_scheduler.step()
 
-        if cfg.steps_before_save and (batch_i % cfg.steps_before_save) == 0:
-            batch_tqdm.set_description(f'Saving batch {batch_i}, loss {loss["loss"]:.2f}')
-            save_model(model, pl.Path(cfg.checkpoint_dir), checkpoint_i, batch_i,
-                        retain_n=cfg.get('retain_n_checkpoints', None))
-        
-        # if cfg.steps_before_eval and (batch_i % cfg.steps_before_save) == 0:
-        #     eval
-
-    save_model(model, pl.Path(cfg.checkpoint_dir), checkpoint_i, batch_i,
-                retain_n=cfg.get('retain_n_checkpoints', None))
-
+if __name__ == '__main__':
+    run()
--- scripts/train_pcd_vae.py
+++ scripts/train_pcd_vae.py
@@ -2,18 +2,22 @@ import torch
 from nocs_diffusion import train, ConfigDirectoriesManager, ConfigLoader
 from diffusers.optimization import get_cosine_schedule_with_warmup
 
-if __name__ == '__main__':
-    cfg_path = ConfigDirectoriesManager()['train_depth_vae.yaml']
-    loader = ConfigLoader.from_config_path(str(cfg_path))
-    dataloader = loader.dataloader
-    model = loader.model
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+@hydra.main(version_base=None, config_path='../src/nocs_diffusion/config', config_name='train_depth_vae')
+def run(cfg: DictConfig) -> None:
+    model = hydra.utils.instantiate(cfg.model).to(cfg.device)
+    dataloader = hydra.utils.instantiate(cfg.dataloader).to(cfg.device)
 
     # TODO: move optimizer to hydra config.
-    optimizer = torch.optim.AdamW(model.parameters(), lr=loader.cfg.lr)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.lr)
     lr_scheduler = get_cosine_schedule_with_warmup(
         optimizer=optimizer,
-        num_warmup_steps=loader.cfg.lr_warmup_steps,
-        num_training_steps=(loader.cfg.num_steps),
+        num_warmup_steps=cfg.lr_warmup_steps,
+        num_training_steps=(cfg.num_steps),
     )
-    train(loader.cfg, model, optimizer, lr_scheduler, dataloader)
-    
+    train(cfg, model, optimizer, lr_scheduler, dataloader)
+
+if __name__ == '__main__':
+    run()
