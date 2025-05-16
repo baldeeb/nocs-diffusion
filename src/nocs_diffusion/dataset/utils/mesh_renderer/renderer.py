73bec71c84d1d16edf13d273c58321eade243685
baldeeb <baldeeb@umich.edu>
2025-05-15 23:15:36 -0400

    Updates MeshRenderer to assume object is colorized.

--- src/nocs_diffusion/dataset/utils/mesh_renderer/renderer.py
+++ src/nocs_diffusion/dataset/utils/mesh_renderer/renderer.py
@@ -10,18 +10,24 @@ import torch
 from pytorch3d.renderer import (
     FoVPerspectiveCameras, 
     RasterizationSettings, 
-    # MeshRenderer, 
     MeshRendererWithFragments,
     MeshRasterizer, 
-    SoftPhongShader,
-    # TexturesVertex,
-    # PerspectiveCameras
+    HardGouraudShader,
+    AmbientLights,
 )
+from pytorch3d.renderer.materials import Materials
 from pytorch3d.renderer.cameras import look_at_view_transform
 
 
 class MeshRenderer:
-    def __init__(self, image_size=256):
+    def __init__(self, image_size=256, get_intrinsics=False, get_points=False):
+        """
+            image_size: int, size of the image to be rendered
+            get_intrinsics: bool, whether to return the camera intrinsics
+            get_points: bool, whether to return the 3D points
+        """
+        self.get_intrinsics = get_intrinsics
+        self.get_points = get_points
         self.image_size = image_size
 
     def __call__(self, meshes, R=None, T=None, scale=None):
@@ -55,67 +61,70 @@ class MeshRenderer:
         cameras = FoVPerspectiveCameras(device=self.device, 
                                         R=R, T=T)
         
-        # Create a phong renderer by composing a rasterizer and a shader
-        renderer = MeshRendererWithFragments(
-            rasterizer=MeshRasterizer(cameras=cameras, 
-                                    raster_settings=raster_settings),
-            shader=SoftPhongShader(device=self.device, cameras=cameras)
-        )
+        
+        # Create a renderer
+        rasterizer = MeshRasterizer(cameras=cameras, 
+                                    raster_settings=raster_settings)
+        lighting = AmbientLights(device=self.device)
+        materials = Materials(device=self.device,
+                              specular_color=[[0.0, 0.0, 0.0]],
+                              shininess=0.0,
+                              ambient_color=((1, 1, 1),),
+                              diffuse_color=[[1.0, 1.0, 1.0]])
+        shader = HardGouraudShader(device=self.device,
+                                   cameras=cameras,
+                                   lights=lighting,
+                                   materials=materials)
+        renderer = MeshRendererWithFragments(rasterizer=rasterizer,
+                                             shader=shader)
 
         # Extract Depth
-        _, fragments = renderer(meshes)
+        nocs_images, fragments = renderer(meshes)
         depth_images = fragments.zbuf[:,:,:, :1]
 
         # Binary mask
         mask_images = (depth_images.permute(0,3,1,2) > 0)   # (b, 1, h, w)
 
-        # Get 3D coordinates
-        ## Get ij frame coordinates
-        b, h, w = depth_images.shape[0:3]
-        i, j = torch.meshgrid(torch.arange(h), torch.arange(w), indexing='ij') 
-        center = torch.tensor([h/2.0, w/2.0]).view(2, 1, 1).to(self.device)
-        ji = torch.stack([j, i], dim=0).to(self.device).float()
-        
-        ## Get normalized coordinates (check pytorch3d docs)
-        ndc = (ji / center) - 1.0
-        ndc = ndc[None].repeat(num_views, 1, 1, 1).permute(0, 2, 3, 1)
-        ndc = torch.concatenate([ndc, depth_images.clone()], dim=-1)
-        
-        ## Transform NDC to world
-        world_coordinates = cameras.unproject_points(ndc.view(b,-1, 3))
+        # Get world to view transforms
         world_to_view_transforms = cameras.get_world_to_view_transform()
-        camera_coordinates = world_to_view_transforms.transform_points(world_coordinates)
-        
-        ## Reshape to images
-        world_coordinates  = world_coordinates.view(b, h, w, 3)
-        camera_coordinates = camera_coordinates.view(b, h, w, 3)
 
-        ## TODO: there are redundant calculations that can be avoided. Look at processing done in dataloader.
-        torch.stack([j, i], dim=0).to(self.device).float()
-        camera_coordinates = torch.concatenate(
-            [camera_coordinates,
-             i[None, :, :, None].expand(b, h, w, 1).to(self.device),
-             j[None, :, :, None].expand(b, h, w, 1).to(self.device)], 
-            dim=-1)
-        ## TODO: do not recalculate the mask here. 
-        camera_pts = [camera_coordinates[i, 
-                                         mask_images[i]\
-                                            .permute(1,2,0)\
-                                            .expand(h, w, 5)
-                                        ].view(-1, 5)
-                      for i in range(b)]
-        perspective_2d_idxs = [c[:, -2:].long() for c in camera_pts]
-        camera_pts = [c[:, :3] for c in camera_pts]
+        if self.get_points:
+            # Get 3D coordinates
+            ## Get ij frame coordinates
+            b, h, w = depth_images.shape[0:3]
+            i, j = torch.meshgrid(torch.arange(h), torch.arange(w), indexing='ij') 
+            center = torch.tensor([h/2.0, w/2.0]).view(2, 1, 1).to(self.device)
+            ji = torch.stack([j, i], dim=0).to(self.device).float()
+            
+            ## Get normalized coordinates (check pytorch3d docs)
+            ndc = (ji / center) - 1.0
+            ndc = ndc[None].repeat(num_views, 1, 1, 1).permute(0, 2, 3, 1)
+            ndc = torch.concatenate([ndc, depth_images.clone()], dim=-1)
+            
+            ## Transform NDC to world
+            world_coordinates = cameras.unproject_points(ndc.view(b,-1, 3))
+            camera_coordinates = world_to_view_transforms.transform_points(world_coordinates)
+            
+            ## Reshape to images
+            world_coordinates  = world_coordinates.view(b, h, w, 3)
+            camera_coordinates = camera_coordinates.view(b, h, w, 3)
 
-
-        # Normalize NOCS images and set background to 1.0
-        nocs_images = world_coordinates.permute(0, 3, 1, 2) + 0.5  # (b, 3, h, w)
-        nocs_images[mask_images.expand(b,3,h,w) == 0] = 1.0  # this seems to help in trainig.
-
-        # Clamp to 0-1 to ignore minor float errors
-        if scale is not None:
-            nocs_images /= scale[:, None, None, None]
-        nocs_images = torch.clamp(nocs_images, 0.0, 1.0)
+            ## TODO: there are redundant calculations that can be avoided. Look at processing done in dataloader.
+            torch.stack([j, i], dim=0).to(self.device).float()
+            camera_coordinates = torch.concatenate(
+                [camera_coordinates,
+                i[None, :, :, None].expand(b, h, w, 1).to(self.device),
+                j[None, :, :, None].expand(b, h, w, 1).to(self.device)], 
+                dim=-1)
+            ## TODO: do not recalculate the mask here. 
+            camera_pts = [camera_coordinates[i, 
+                                            mask_images[i]\
+                                                .permute(1,2,0)\
+                                                .expand(h, w, 5)
+                                            ].view(-1, 5)
+                        for i in range(b)]
+            perspective_2d_idxs = [c[:, -2:].long() for c in camera_pts]
+            camera_pts = [c[:, :3] for c in camera_pts]
 
         # Permute and mask depth
         depth_images = depth_images.permute(0,3,1,2) # (b, 1, h, w)
@@ -127,14 +136,17 @@ class MeshRenderer:
         # #  organized in the object cloud 
         # perspective_2d_idxs = [torch.where(d) for d in depth_images]
 
-        return {"nocs_images":  nocs_images, 
-                "depth_images": depth_images,
-                "mask_images":  mask_images,
-                "face_points":  camera_pts,
-                "transforms": world_to_view_transforms.inverse(),
-                "face_pts_2d_idxs": perspective_2d_idxs,
-                "projection_matrix": cameras.get_projection_transform()
-                }
+        render = {  "nocs_images":  nocs_images, 
+                    "depth_images": depth_images,
+                    "mask_images":  mask_images,
+                    "transforms": world_to_view_transforms.inverse()}
+        if self.get_points:
+            render.update({ "face_points":  camera_pts,
+                            "face_pts_2d_idxs": perspective_2d_idxs})
+
+        if self.get_intrinsics:
+            render.update({ "projection_matrix": cameras.get_projection_transform() })
+        return render
 
     def generate_random_camera_viewing_angles(self, count=1):
         # return 1.20, 45.0, 225.0
